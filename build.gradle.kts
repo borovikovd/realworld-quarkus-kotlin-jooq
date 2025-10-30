288969fcfd9ac20404a5bb6e00b133e9f4510247
Denis Borovikov <denis.borovikov@gmail.com>
2025-10-30 10:40:54 +0100

    Downgrade versions for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 // Plugins
 // ============================================
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.allopen") version "2.2.21"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.allopen") version "2.2.20"
     id("io.quarkus")
     id("org.openapi.generator") version "7.17.0"
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
