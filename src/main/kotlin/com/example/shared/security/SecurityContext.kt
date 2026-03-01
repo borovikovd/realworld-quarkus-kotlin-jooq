8142a6fa36d4b790729e476d7b22374ea0a59bb2
Denis Borovikov <denis.borovikov@gmail.com>
2026-03-01 13:35:47 +0100

    refactor: use constructor injection in SecurityContext

--- src/main/kotlin/com/example/shared/security/SecurityContext.kt
+++ src/main/kotlin/com/example/shared/security/SecurityContext.kt
@@ -2,14 +2,12 @@ package com.example.shared.security
 
 import com.example.shared.exceptions.UnauthorizedException
 import jakarta.enterprise.context.RequestScoped
-import jakarta.inject.Inject
 import org.eclipse.microprofile.jwt.JsonWebToken
 
 @RequestScoped
-class SecurityContext {
-    @Inject
-    lateinit var jwt: JsonWebToken
-
+class SecurityContext(
+    private val jwt: JsonWebToken,
+) {
     val currentUserId: Long?
         get() = if (jwt.subject != null) jwt.subject.toLongOrNull() else null
 
